a0cc2f83375fee7820609b91f732bd5963ba0b0d
Ezekiel <geniuswealthb@gmail.com>
2025-12-29 12:10:16 +0100

    Setup react query

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
 
-const QueryProvider = () => {
+"use client";
+import { client } from "@/app/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+
+export default function QueryProvider({ children }: { readonly children: React.ReactNode }) {
     return (
-        <div>QueryProvider</div>
-    )
+        <QueryClientProvider client={client.get()}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
-
-export default QueryProvider
